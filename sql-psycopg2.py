7490b367500d3ef312cb3d34e865c90e0f4632dd
Russ-S <rstanleym74@gmail.com>
2023-05-31 11:55:34 +0000

    add database queries

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -6,6 +6,22 @@ connection = psycopg2.connect(database= "chinook")
 # build a cursor object of the database
 cursor = connection.cursor()
 
+# # Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# # Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# # Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# # Query 4 - select only "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with "ArtistId" #51 from the "Album" table
+cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+
 # fetch the results (multiple)
 results = cursor.fetchall()
 
