b3943cfa45180ab417e899b530dcdfea0cb6a324
Russ-S <rstanleym74@gmail.com>
2023-05-31 17:19:08 +0000

    create queries in sql-orm

--- sql-orm.py
+++ sql-orm.py
@@ -7,17 +7,17 @@ from sqlalchemy.orm import sessionmaker
 
 
 # executing the instuctions from the "chinook" database
-db = create_engine("postgresql///chinook")
+db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 # create a class-based model for the "Artist" table
-class ArtistTable(base):
+class Artist(base):
     __tablename__ = "Artist"
     ArtistId = Column(Integer, primary_key=True)
     Name = Column(String)
 
 # create a class-based model for the "Album" table
-class AlbumTable(base):
+class Album(base):
     __tablename__ = "Album"
     AlbumId = Column(Integer, primary_key=True)
     Title = Column(String)
@@ -46,3 +46,30 @@ session = Session()
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
 
+# Query 1 - select all records from the "Artist" Table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
+
+# Query 3 - select only "Queen" from the "Artist" table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 4 - select only "ArtistId" #51 from the "Artist" table
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 5 - select only the albums with "ArtistId" #51 from the "Album" table
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
+
+# Query 6 - select all tracks where the "Composer" is "Queen" from the "Track" table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(track.TrackId, track.Name, track.AlbumId, track.Composer, sep=" | ")
